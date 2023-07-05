15ab3261c0dc57caa18b6ac856c54b42a95002ff
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2023-07-05 11:22:45 +0800

    加参数类型R、V、回调用于与外壳的交互。r语言项为rust开发库的极简参数方式。不指定实际函数名时以函数名为名

--- src/funcs_.rs
+++ src/funcs_.rs
@@ -1,103 +1,66 @@
 use zhscript2::{u_::*, as_ref__, as_mut_ref__};
-use std::{mem, ffi::CStr, str, fmt};
+use super::g_;
+use std::{mem, ffi::{CStr}, str, fmt};
 use libloading::{Library, os::unix as limp_};
 
-mod call_;
-use call_::{IntoArg, Ptr_};
+#[cfg(feature = "i8_u8")]
+type I8_ = u8;
+#[cfg(not(feature = "i8_u8"))]
+type I8_ = i8;
 
-#[derive(PartialEq, Clone, Debug)]
-pub enum Typ_ {
-	Void, Int, UInt, Long, ULong, Float, Char, CharPtr, CharAddr, Addr, NBool,
-	Unsigned, Ptr, Val,
-	IntVal(i64), UIntVal(u64), StringVal(String),
-	Buf, BufSiz, Addrret, Errret, Z,
-	No, Err
+mod call_;
+pub use call_::{IntoArg, Ptr_};
+pub mod callback_;
+pub mod typ_;
+use typ_::*;
+
+fn s__<'a>(i:*const I8_) -> &'a str {
+	if i as usize == 0 {
+		"NULL"
+	} else {unsafe {
+		CStr::from_ptr(i).to_str().unwrap()
+	}}
 }
 
 pub type RI_ = Rc_<Item_>;
 
-pub type Ptr2_ = fn(env:&code_::Env_) -> Result2_;
+pub type Ptr2_ = fn(&code_::Env_) -> Result2_;
+pub type Ptr3_ = fn(&[String], &mut Vec<String>) -> Result<(), String>;
 
 #[derive(Debug)]
 pub enum Ptr__ {
 	Ptr,
 	Ptr2,
+	Ptr3,
 }
 
 pub struct Item_ {
 	p_:Ptr__,
 	p_0_:Option<Ptr_>,
 	p_1_:Option<Ptr2_>,
+	p_2_:Option<Ptr3_>,
 	argv_:Vec<Typ_>,
 	ret_:Typ_,
 }
 
 impl Item_ {
-	fn new(p:Ptr_, args:&str) -> Result<Self, String> {
-		Self::new3(Ptr__::Ptr, Some(p), None, args)
+	pub fn new(p:Ptr_, args:&str) -> Result<Self, String> {
+		Self::new__(Ptr__::Ptr, Some(p), None, None, args)
 	}
-	fn new2(p:Ptr2_, args:&str) -> Result<Self, String> {
-		Self::new3(Ptr__::Ptr2, None, Some(p), args)
+	pub fn new2(p:Ptr2_, args:&str) -> Result<Self, String> {
+		Self::new__(Ptr__::Ptr2, None, Some(p), None, args)
 	}
-	fn new3(p_:Ptr__, p_0_:Option<Ptr_>, p_1_:Option<Ptr2_>, args:&str) -> Result<Self, String> {
+	pub fn new3(p:Ptr3_, args:&str) -> Result<Self, String> {
+		Self::new__(Ptr__::Ptr3, None, None, Some(p), args)
+	}
+	fn new__(p_:Ptr__, p_0_:Option<Ptr_>, p_1_:Option<Ptr2_>, p_2_:Option<Ptr3_>,
+			args:&str) -> Result<Self, String> {
 		let mut argv_ = vec![];
 		let mut ret_ = Typ_::Void;
-		Self::pargs__(args, &mut ret_, &mut argv_)?;
-		Ok(Self {p_, p_0_, p_1_, argv_, ret_})
+		pargs__(args, &mut ret_, &mut argv_)?;
+		Ok(Self {p_, p_0_, p_1_, p_2_, argv_, ret_})
 	}
-	
-	fn typ__(c:u8) -> Typ_ {
-		match c {
-			b'i' => Typ_::Int,
-			b'l' => Typ_::Long,
-			b'f' => Typ_::Float,
-			b'c' => Typ_::Char,
-			b'u' => Typ_::Unsigned,
-			b'*' => Typ_::Ptr,
-			b'&' => Typ_::Addr,
-			b'n' => Typ_::NBool,
-			b':' => Typ_::Val,
-			b'B' => Typ_::Buf,
-			b'S' => Typ_::BufSiz,
-			b'A' => Typ_::Addrret,
-			b'E' => Typ_::Errret,
-			b'Z' => Typ_::Z,
-			b'-' => Typ_::No,
-			_ => Typ_::Err,
-		}
-	}
-	
-	fn typ3__(t2:&mut [Typ_], s2:&mut String) -> Typ_ {
-		let mut ret = Typ_::Err;
-		if Typ_::No == t2[1] {
-			ret = t2[0].clone();
-		} else {
-			if t2[0] == Typ_::Char && t2[1] == Typ_::Ptr {
-				ret = Typ_::CharPtr
-			} else if t2[1] == Typ_::Int && t2[0] == Typ_::Unsigned {
-				ret = Typ_::UInt
-			} else if t2[0] == Typ_::Long && t2[1] == Typ_::Unsigned {
-				ret = Typ_::ULong
-			} else if t2[0] == Typ_::Int && t2[1] == Typ_::Val {
-				if let Ok(i) = s2.parse::<i64>() {
-					ret = Typ_::IntVal(i)
-				}
-			} else if t2[0] == Typ_::Unsigned && t2[1] == Typ_::Val {
-				if let Ok(i) = s2.parse::<u64>() {
-					ret = Typ_::UIntVal(i)
-				}
-			} else if t2[0] == Typ_::Ptr && t2[1] == Typ_::Val {
-				ret = Typ_::StringVal(s2.to_string())
-			} else if t2[0] == Typ_::Char && t2[1] == Typ_::Addr {
-				ret = Typ_::CharAddr
-			}
-			t2[1] = Typ_::No;
-		}
-		t2[0] = Typ_::Void;
-		s2.clear();
-		ret
-	}
-	
+
 	pub fn call__(&self, argv2:&Vec<Typ_>, args:&result_::List_, start:usize, env:&code_::Env_) -> Result2_ {
 		match self.p_ {
 			Ptr__::Ptr => {
@@ -105,11 +68,13 @@ impl Item_ {
 				let mut args2: Vec<usize> = vec![];
 				let mut fargs2: Vec<f64> = vec![];
 				let mut args_0 = vec![];
-				let mut buf = vec![0i8; 512]; //1953
+				let mut buf = vec![0 as I8_; 512]; //1953
 				let mut errret = 0;
 				let errret2:*mut i32 = &mut errret;
+				let mut ret2:Vec<*mut result_::List_> = vec![];
+				let env2:*mut code_::Env_ = Box::into_raw(Box::new(code_::Env_::new10(env)));
+				let ret3:*mut Result2_ = Box::into_raw(Box::new(ok__()));
 				{
-					let mut idx = 0;
 					let mut push_s__ = |i:&str, args2:&mut Vec<usize>| {
 						self.push__(if i.ends_with("\0") {
 							i.as_ptr()
@@ -119,6 +84,7 @@ impl Item_ {
 							args_0[args_0.len() - 1].as_ptr()
 						}, args2)
 					};
+					let mut idx = 0;
 					let mut args2__ = |argv:&Vec<Typ_>| {
 						//println!("{:?}", argv);
 						for t in argv {
@@ -159,10 +125,32 @@ impl Item_ {
 									}
 									continue
 								}
+								Typ_::Ret => {
+									let i = || {Box::into_raw(Box::new(result_::List_::new()))};
+									ret2.push(i());
+									self.push__(ret2[ret2.len() - 1], &mut args2);
+									continue
+								}
+								Typ_::Callback(i) => {
+									match i.as_str() {
+										"" => {
+											self.push__(callback_::cb__ as u64, &mut args2);
+											self.push__(callback_::free__ as u64, &mut args2);
+											self.push__(env2, &mut args2);
+											self.push__(ret3, &mut args2);
+										}
+										"add" | "A" => {
+											self.push__(callback_::add__ as u64, &mut args2);
+											self.push__(env2, &mut args2);
+										}
+										_ => return no_typ__(t, ' ')
+									}
+									continue
+								}
 								_ => {}
 							}
 							if idx >= args.len() {
-								return result2_::err2__("参数不足")
+								return g_::err__('<')
 							}
 							let i = &args[idx];
 							match t {
@@ -171,9 +159,9 @@ impl Item_ {
 								Typ_::Long => {self.push2__::<i64>(i, &mut args2)?}
 								Typ_::ULong | Typ_::Addr => {self.push2__::<u64>(i, &mut args2)?}
 								Typ_::Float => {self.push3__::<f64>(i, &mut fargs2, &mut args2)?}
-								Typ_::Char => self.push__(if i.is_empty() {0} else {i.as_bytes()[0]}, &mut args2),
+								Typ_::Char => self.push__(if i.is_empty() {0} else {let a = i.as_bytes(); a[a.len() - 1]}, &mut args2),
 								Typ_::CharPtr => push_s__(i, &mut args2),
-								_ => return result2_::err__(format!("参数类型 {:?} 未实现", t))
+								_ => return no_typ__(t, ' ')
 							}
 							idx += 1;
 						}
@@ -182,10 +170,10 @@ impl Item_ {
 					args2__(&self.argv_)?;
 					args2__(&argv2)?;
 					if idx < args.len() {
-						return result2_::err2__("参数超量")
+						return g_::err__('>')
 					}
 				}
-				
+
 				let mut ret_low: usize = 0;
 				let mut ret_high: usize = 0;
 				let mut ret_float: f64 = 0.0;
@@ -199,21 +187,53 @@ impl Item_ {
 					Typ_::Long => ret.add__(ret_low as i64),
 					Typ_::ULong | Typ_::Addr => ret.add__(ret_low as u64),
 					Typ_::Float => ret.add__(ret_float),
-					Typ_::CharPtr => ret.add__(self.s__(ret_low as *const i8)),
+					Typ_::CharPtr => ret.add__(s__(ret_low as *const I8_)),
 					Typ_::NBool => ret.add__(if ret_low == 0 {"1"} else {"0"}),
-					_ => return result2_::err__(format!("返回类型 {:?} 不支持", self.ret_))
+					_ => return no_typ__(&self.ret_, 'r')
 				}
-				for t in &self.argv_ {
-					match t {
-						Typ_::Buf | Typ_::BufSiz => ret.add__(self.s__(buf.as_ptr())),
-						Typ_::Errret => {
-							if errret != 0 {
-								return result2_::err__(format!("{} 错误码{}", self.s__(buf.as_ptr()), errret))
+				{
+					let mut by_err_use = false;
+					for t in &self.argv_ {
+						match t {
+							Typ_::Errret => {
+								if errret > 0 && errret <= result2_::ERR_ {
+									return result2_::err__(format!("{} 错误码{}", s__(buf.as_ptr()), errret))
+								}
+								by_err_use = true;
 							}
+							Typ_::Buf | Typ_::BufSiz => {
+								loop {
+									if by_err_use && buf[0] == 0 {
+										break
+									}
+									if self.ret_ != Typ_::Void {
+										as_ref__!(env.w).dunhao__(&mut ret)
+									}
+									ret.add__(s__(buf.as_ptr()));
+									break;
+								}
+							}
+							_ => {}
 						}
-						_ => {}
 					}
 				}
+				unsafe {
+					for i in ret2 {
+						let ret2 = Box::from_raw(i);
+						for i in ret2.a_ {
+							ret.add4__(i)
+						}
+					}
+					drop(Box::from_raw(env2));
+					let ret3 = Box::from_raw(ret3);
+					if ret3.is_err() {
+						match errret {
+							callback_::NO_ERR_RETURN /*| callback_::NO_ERR_BREAK*/ => {}
+							_ => return *ret3
+						}
+					}
+				}
+				return Ok(())
 			}
 			Ptr__::Ptr2 => {
 				let q2 = Qv_::new2(Some(env.q.clone()));
@@ -226,12 +246,14 @@ impl Item_ {
 								match t {
 									Typ_::StringVal(i) => {
 										args2.add__(i);
+										if idx < args.len() {
+											as_ref__!(env.w).dunhao__(args2)
+										}
 										continue
 									}
 									Typ_::Z => {
-										let len = args.len();
-										while idx < len {
-											args2.push(args[idx].clone());
+										while idx < args.len() {
+											args2.add4__(args[idx].clone());
 											idx += 1;
 										}
 										continue
@@ -239,12 +261,12 @@ impl Item_ {
 									_ => {}
 								}
 								if idx >= args.len() {
-									return result2_::err2__("参数不足")
+									return g_::err__('<')
 								}
 								let i = &args[idx];
 								match t {
-									Typ_::CharPtr => args2.push(i.clone()),
-									_ => return result2_::err__(format!("参数类型 {:?} 未实现", t))
+									Typ_::CharPtr => args2.add4__(i.clone()),
+									_ => return no_typ__(t, ' ')
 								}
 								idx += 1;
 							}
@@ -253,29 +275,48 @@ impl Item_ {
 						args2__(&self.argv_)?;
 						args2__(&argv2)?;
 						if idx < args.len() {
-							return result2_::err2__("参数超量")
+							return g_::err__('>')
 						}
 					}
 				}
+				//#[cfg(debug_assertions)] //居然致命？以下不得删
 				if as_ref__!(env.w).dbg_.arg_ {
 					as_ref__!(env.w).dbg_.arg__(&as_ref__!(q2.args_));
 				}
 				self.p_1_.unwrap()(&code_::Env_::new2(t__(q2), env))?;
 				match self.ret_ {
 					Typ_::Void => {}
-					_ => return result2_::err__(format!("返回类型 {:?} 不支持", self.ret_))
+					_ => return no_typ__(&self.ret_, 'r')
+				}
+			}
+			Ptr__::Ptr3 => {
+				{
+					let args2__ = |argv:&Vec<Typ_>| {
+						for t in argv {
+							return no_typ__(t, ' ')
+						}
+						ok__()
+					};
+					args2__(&self.argv_)?;
+					args2__(&argv2)?;
+				}
+				let mut ret2 = vec![];
+				if let Err(err) = self.p_2_.unwrap()(&args.to_vec5__(start), &mut ret2) {
+					return result2_::err__(err)
+				}
+				let mut ret = as_mut_ref__!(env.ret);
+				let mut first = true;
+				for i in ret2 {
+					if first {
+						first = false
+					} else {
+						as_ref__!(env.w).dunhao__(&mut ret)
+					}
+					ret.add__(i);
 				}
 			}
 		}
 		Ok(())
-	}
-	
-	fn s__<'a>(&self, i:*const i8) -> &'a str {
-		if i as usize == 0 {
-			"NULL"
-		} else {unsafe {
-			CStr::from_ptr(i).to_str().unwrap()
-		}}
 	}
 
 	fn push__<I:IntoArg>(&self, i:I, args2:&mut Vec<usize>) {
@@ -288,7 +329,7 @@ impl Item_ {
 			self.push__(i, args2);
 			ok__()
 		} else {
-			result2_::err__(format!("{} 非数字", s))
+			g_::err2__(s, 'n')
 		}
 	}
 	fn push3__<F>(&self, s:&str, fargs2:&mut Vec<f64>, args2:&mut Vec<usize>) -> Result2_
@@ -304,108 +345,8 @@ impl Item_ {
 			}
 			ok__()
 		} else {
-			result2_::err__(format!("{} 非数字", s))
+			g_::err2__(s, 'n')
 		}
-	}
-	
-	fn pargs__(args:&str, ret_:&mut Typ_, argv_:&mut Vec<Typ_>) -> Result<(), String> {
-		let mut t2 = [Typ_::Void, Typ_::No];
-		let mut i2 = 0;
-		
-		let mut first = true;
-		let mut idx = 0;
-		let mut s2 = String::new();
-		let args = args.to_string() + "-";
-		let cs = args.as_bytes();
-		'l1: while idx < cs.len() {
-			let c = cs[idx];
-			let t = Self::typ__(c);
-			if Typ_::Err == t {
-				return Err(format!("{} 非参数类型符", c as char))
-			}
-			if first {
-				match t {
-					Typ_::No => {
-						*ret_ = Self::typ3__(&mut t2, &mut s2);
-						i2 = 0;
-						first = false
-					}
-					_ => Self::typ2__(t, &mut t2, &mut i2)?
-				}
-			} else {
-				match t {
-					Typ_::No => {
-						argv_.push(Self::typ3__(&mut t2, &mut s2));
-						i2 = 0;
-					}
-					_ => Self::typ2__(t, &mut t2, &mut i2)?
-				}
-			}
-			if t2[1] == Typ_::Val {
-				match t2[0] {
-					Typ_::Int | Typ_::Unsigned => {
-						let mut hao = t2[0] == Typ_::Int;
-						loop {
-							idx += 1;
-							if idx >= cs.len() {
-								return Err(format!("{}位置意外", idx))
-							}
-							let c = cs[idx];
-							if hao {
-								hao = false;
-								match c {
-									b'-' | b'+' => {
-										s2.push(c as char);
-										continue
-									}
-									_ => {}
-								}
-							}
-							match c {
-								b'0'..=b'9' => s2.push(c as char),
-								_ => continue 'l1
-							}
-						}
-					}
-					Typ_::Ptr => {
-						let mut hao = true;
-						let mut hao2 = 0;
-						loop {
-							idx += 1;
-							if idx >= cs.len() {
-								return Err(format!("{}位置意外", idx))
-							}
-							let c = cs[idx];
-							if hao {
-								hao = false;
-								hao2 = c;
-								continue
-							}
-							if c == hao2 {
-								break
-							}
-							s2.push(c as char)
-						}
-					}
-					_ => {}
-				}
-			}
-			idx += 1
-		}
-		Ok(())
-	}
-	pub fn pargs2__(args:&str, argv2:&mut Vec<Typ_>) -> Result<(), String> {
-		let mut ret = Typ_::No;
-		Self::pargs__(args, &mut ret, argv2)
-	}
-	
-	fn typ2__(t:Typ_, t2:&mut [Typ_], i2:&mut usize) -> Result<(), String> {
-		if *i2 >= t2.len() {
-			return Err(format!("{}>={} len 错误参数格式", *i2, t2.len()))
-		}
-		t2[*i2] = t;
-		*i2 += 1;
-		Ok(())
 	}
 }
 
@@ -443,24 +384,34 @@ impl List_ {
 		Ok(Self {lib_:Library::from(limp_::Library::this()), a_:vec![], lang_})
 	}
 	
-	pub fn add__(&mut self, vals:&[String]) -> Result<RI_, String> {
+	pub fn add__(&mut self, vals:&[String], names:&[String]) -> Result<RI_, String> {
 		unsafe {
+			let args = if vals.len() > 1 {&vals[1]} else {""};
+			let sym  = if vals.len() > 0 {&vals[0]} else {""};
+			let sym = if sym.is_empty() {&names[0]} else {sym};
 			let ri = match self.lang_.as_str() {
-				"rust" => match self.lib_.get::<Ptr2_>(vals[0].as_bytes()) {
+				"rust" => match self.lib_.get::<Ptr2_>(sym.as_bytes()) {
 					Ok(p) => {
-						let i = Item_::new2(*p, &vals[1])?;
+						let i = Item_::new2(*p, args)?;
 						Rc_::new(i)
 					}
 					Err(e) => return Err(e.to_string())
 				},
-				"c" => match self.lib_.get::<fn()>(vals[0].as_bytes()) {
+				"r" => match self.lib_.get::<Ptr3_>(sym.as_bytes()) {
 					Ok(p) => {
-						let i = Item_::new(*p.into_raw() as Ptr_, &vals[1])?;
+						let i = Item_::new3(*p, args)?;
+						Rc_::new(i)
+					}
+					Err(e) => return Err(e.to_string())
+				},
+				"c" => match self.lib_.get::<fn()>(sym.as_bytes()) {
+					Ok(p) => {
+						let i = Item_::new(*p.into_raw() as Ptr_, args)?;
 						Rc_::new(i)
 					}
 					Err(e) => return Err(e.to_string())
 				}
-				_ => return Err(format!("不支持 {}", self.lang_))
+				_ => return Err(format!("不支持 {} 语言", self.lang_))
 			};
 			self.a_.push(ri.clone());
 			Ok(ri)
