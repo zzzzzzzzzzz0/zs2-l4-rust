f24b743e6ae66b2819776efa079856e253a1e6e7
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2023-07-05 11:23:05 +0800

    响应no_dl

--- src/i_.rs
+++ src/i_.rs
@@ -1,9 +1,12 @@
-use zhscript2::{u_::*, as_mut_ref__};
+use zhscript2::{u_::*, as_mut_ref__, cfg_if};
 use super::*;
+
+cfg_if! {
+	if #[cfg(not(feature = "no_dl"))] {
 
 struct U11_ {}
 impl pars_::U11_ for U11_ {
-	fn u11__(&self, kw:keyword_::RI_, kws:&keyword_::List_, _codes:& code_::List_) -> code_::OI_ {
+	fn u11__(&self, kw:keyword_::RI_, kws:&keyword_::List_, _:& code_::List_) -> code_::OI_ {
 		if kw.s_ == dl_::NAME_ {
 			return code_::oi__(dl_::Item_::new(kw))
 		}
@@ -17,14 +20,22 @@ impl pars_::U11_ for U11_ {
 	}
 }
 
+}}
+
 pub fn w__() -> world_::T_ {
 	let w = t__(World_::new());
-	{
-		let kws = &mut as_mut_ref__!(w).kws_;
-		kws.add__ (  dl_::NAME_, keyword_::Id_::U11);
-		kws.add3__(func_::NAME_, keyword_::Id_::U11, keyword_::Grp_ {set_:true, ..Default::default()});
-		kws.add__ (call_::NAME_, keyword_::Id_::U11);
+	cfg_if! {
+		if #[cfg(not(feature = "no_dl"))] {
+			{
+				let kws = &mut as_mut_ref__!(w).kws_;
+				kws.add__ (  dl_::NAME_, keyword_::Id_::U11);
+				kws.add3__(func_::NAME_, keyword_::Id_::U11, keyword_::Grp_ {set_:true, ..Default::default()});
+				kws.add__ (call_::NAME_, keyword_::Id_::U11);
+
+				kws.add__("为", keyword_::Id_::Dunhao);
+			}
+			as_mut_ref__!(w).pars_.u11_ = Some(Rc_::new(U11_ {}));
+		}
 	}
-	as_mut_ref__!(w).pars_.u11_ = Some(Rc_::new(U11_ {}));
 	w
 }
